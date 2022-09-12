b986529445d850b0cba1e136fde63e13e05387ab
ShaikShamee <shameema.shaik78@gmail.com>
2022-09-12 11:10:34 +0530

    changes

---
+++ Practice/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace Practice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            string answer = "y";
+            for (; answer == "y";)
+            {
+
+                string filepath = @"D:\\Employee.txt";
+                    if (!File.Exists(filepath))
+                    {
+
+                        using (StreamWriter streamWriter = File.CreateText(filepath))
+                        {
+                            streamWriter.WriteLine($"Record : EmpId - {employee.EmpId}, Name - {employee.EmpName}, Age - {employee.Age}, Gender - {employee.Gender}, Salary - {employee.Salary}");
+                            Console.WriteLine("file created successfully");
+                        }
+                    }
+                    else
+                    {
+                        using (StreamWriter streamWriter = File.AppendText(filepath))
+                        {
+                            streamWriter.WriteLine($"Record : Id - {employee.EmpId}, Name - {employee.EmpName}, Age - {employee.Age}, Gender - {employee.Gender}, Salary - {employee.Salary}");
+                            Console.WriteLine("file appended successfully");
+                        }
+                    }
+                
+
+
+                Console.Write("Enter employee's id:");
+                employee.EmpId = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter employee's name:");
+                employee.EmpName = Console.ReadLine();
+
+                Console.Write("Enter employee's age:");
+                employee.Age = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter employee's gender:");
+                employee.Gender = Console.ReadLine();
+
+                Console.Write("Enter employee's salary:");
+                employee.Salary = double.Parse(Console.ReadLine());
+
+                SqlConnection scon = new SqlConnection("server=localhost;database=CURD Angular;integrated security=true");
+                //insertion command creation
+                SqlCommand scmd = new SqlCommand("insert into Employee_Table values( " + employee.EmpId + "  , '" + employee.EmpName + "' , " + employee.Age + " , ' " + employee.Gender + " ' ," + employee.Salary + ")", scon);
+                scon.Open();
+                scmd.ExecuteNonQuery();
+                scon.Close();
+                Console.WriteLine("Record Inserted Successfully");
+
+                Console.WriteLine("Do you want to repeat? Y or N");
+                answer = Console.ReadLine();
+
+
+            }
+        }
+
+    }
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public double Salary { get; set; }
+    }
+}
--- Task/Program.cs
+++ Task/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Task
 {
-    class Program
+    [Serializable]
+    public class Employee
     {
+
+       public int Emp_Id;
+        public string Emp_Name;
+
+        public Employee(int id, string name)
+        {
+            Emp_Id = id;
+            Emp_Name = name;
+        }
+    }
+        public class Program
+        { 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //DirectoryInfo directoryInfo = new DirectoryInfo(@"D:\\bhavna");
+            //try
+            //{
+            //    if(directoryInfo.Exists)
+            //    {
+            //        Console.WriteLine("Directory already exsists");
+            //        directoryInfo.Delete();
+            //        Console.WriteLine("Directory deleted successfully");
+            //    }
+            //    else
+            //    {
+            //        directoryInfo.Create();
+            //        Console.WriteLine("Directory created ");
+            //        Console.WriteLine("no directory is exists");
+            //    }
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.ToString());
+            //}
+            FileStream fileStream = new FileStream(@"",FileMode.OpenOrCreate);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            Employee employee = (Employee)binaryFormatter.Deserialize(fileStream);
+
+            Console.WriteLine("Emp Id:"+employee.Emp_Id);
+            Console.WriteLine("Emp Name:"+employee.Emp_Name);
+            Console.ReadLine();
+            
         }
     }
 }
